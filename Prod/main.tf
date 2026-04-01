78084a97942eccb5b21f80ceabd321362e1876d6
@vijay <61233402+VijayNariyal@users.noreply.github.com>
2026-04-02 00:17:44 +0530

    Update main.tf

--- Prod/main.tf
+++ Prod/main.tf
@@ -1,13 +1,13 @@
 module "resource_groups" {
   source = "../RG"
   resource_group_name = "TF-RG"
-  resource_group_location = "East US"
+  resource_group_location = "centralindia"
 }
 
 module "storage_accounts" {
   depends_on = [ module.resource_groups ]
   source = "../STG"
   resource_group_name = "TF-RG"
-  resource_group_location = "East US"
+  resource_group_location = "centralindia"
   storage_account_name = "tfstorage120"
 }
